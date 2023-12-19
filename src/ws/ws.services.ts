0977788dea17b03c8ebfb5ee6ab99ed3ef6c50aa
Oleg Gobyzov <oleg.a.g.post@gmail.com>
2023-12-20 05:39:48 +0700

    closes #25 Bug fixed, server reaction to WS connection without token has been modified

--- src/ws/ws.services.ts
+++ src/ws/ws.services.ts
@@ -27,54 +27,68 @@ export class WSNotificationsService {
     const token = io.handshake.auth.token
       ? io.handshake.auth.token
       : (io.handshake.headers.token as string);
-    const decoded = jwt.verify(token, envConfig.secretPrivate) as ITokenPayload;
-    const user = await this.authService.getUserById(decoded.sub);
-    const userUUID = user?.id;
-    if (!userUUID || !(await this.authService.getUserById(userUUID))) {
-      io.send("Unauthrized socket connection");
-      io.disconnect(true);
-      return;
-    } else {
-      let extractedNotifications =
-        await this.notificationService.getNewNotifications(
-          userUUID,
-          Date.now()
-        );
-      if (extractedNotifications.length > this.maxToSendOnConnect) {
-        extractedNotifications = extractedNotifications.slice(
-          0,
-          this.maxToSendOnConnect
-        );
-      }
-      const scheduledNotifiactionsIds = runningApp.notifierJobs.map((j) => {
-        return j.notification.id;
-      });
-      const notificationsToSend = extractedNotifications.map((e) => {
-        if (scheduledNotifiactionsIds.includes(e.id)) {
-          runningApp.notificationsScheduler.cancelJob(e);
+    try {
+      const decoded = jwt.verify(token, envConfig.secretPrivate) as ITokenPayload;
+      const user = await this.authService.getUserById(decoded.sub);
+      const userUUID = user?.id;
+      if (!userUUID || !(await this.authService.getUserById(userUUID))) {
+        io.send("Unauthrized socket connection");
+        io.disconnect(true);
+        return;
+      } else {
+        let extractedNotifications =
+          await this.notificationService.getNewNotifications(
+            userUUID,
+            Date.now()
+          );
+        if (extractedNotifications.length > this.maxToSendOnConnect) {
+          extractedNotifications = extractedNotifications.slice(
+            0,
+            this.maxToSendOnConnect
+          );
         }
-        const respNotification: INotificationToSendWS = {
-          type: e.type,
-          triggeredAt: e.trigger_at,
-          message: e.message,
-        };
-        return respNotification;
-      });
-      io.emit("notifications", notificationsToSend);
-      await this.notificationService.setSentStatus(
-        extractedNotifications.map((e) => {
-          return e.id;
-        }),
-        true
-      );
-      runningApp.sessions.push({
-        user: userUUID,
-        socket: io,
-      });
-      io.on("disconnect", () => {
-        this.onDisconnect(io);
-      });
-      return userUUID;
+        const scheduledNotifiactionsIds = runningApp.notifierJobs.map((j) => {
+          return j.notification.id;
+        });
+        const notificationsToSend = extractedNotifications.map((e) => {
+          if (scheduledNotifiactionsIds.includes(e.id)) {
+            runningApp.notificationsScheduler.cancelJob(e);
+          }
+          const respNotification: INotificationToSendWS = {
+            type: e.type,
+            triggeredAt: e.trigger_at,
+            message: e.message,
+          };
+          return respNotification;
+        });
+        io.emit("notifications", notificationsToSend);
+        await this.notificationService.setSentStatus(
+          extractedNotifications.map((e) => {
+            return e.id;
+          }),
+          true
+        );
+        runningApp.sessions.push({
+          user: userUUID,
+          socket: io,
+        });
+        io.on("disconnect", () => {
+          this.onDisconnect(io);
+        });
+        return userUUID;
+      }
+    }
+    catch (e) {
+      if (!token) {
+        io.send("Unauthrized socket connection");
+        io.disconnect(true);
+      }
+      else{
+        console.error(e)
+        io.send("Unknown socket error");
+        io.disconnect(true);
+      }
+      return;
     }
   }
 }

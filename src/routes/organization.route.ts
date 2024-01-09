6b0d60cbec72a887e28a73d742551e8fe433445d
Alex Emel <sirsulic@gmail.com>
2024-01-09 06:25:11 +0600

    Added checkEntityAccess middleware #39

--- src/routes/organization.route.ts
+++ src/routes/organization.route.ts
@@ -3,21 +3,37 @@ import { OrganizationController } from '@/controllers/organization.controller';
 import { checkAuth } from '@/middleware/auth.middleware';
 import { checkRole } from '@/middleware/roleChecker.middleware';
 import { ERole } from '@/types/roles';
+import { checkEntityAccess } from '@/middleware/updateChecker.midleware';
 
 export class OrganizationRoute {
-    public path = '/organizations';
-    public router = Router();
-    private controller: OrganizationController;
+  public path = "/organizations";
+  public router = Router();
+  private controller: OrganizationController;
 
-    constructor() {
-        this.controller = new OrganizationController();
-        this.init();
-    }
+  constructor() {
+    this.controller = new OrganizationController();
+    this.init();
+  }
 
-    private init() {
-        this.router.post('/', checkAuth, checkRole([ERole.umanuAdmin]), this.controller.createOrganizationEntry);
-        this.router.get('/:id', checkAuth, this.controller.getOrganizationById);
-        this.router.get('/', checkAuth, this.controller.getAllOrganizations);
-        this.router.put('/:id', checkAuth, this.controller.updateOrganization);
-    }
+  private init() {
+    this.router.post(
+      "/",
+      checkAuth,
+      checkRole([ERole.umanuAdmin]),
+      this.controller.createOrganizationEntry
+    );
+    this.router.get(
+      "/:id",
+      checkAuth,
+      checkEntityAccess("organization"),
+      this.controller.getOrganizationById
+    );
+    this.router.get("/", checkAuth, this.controller.getAllOrganizations);
+    this.router.put(
+      "/:id",
+      checkAuth,
+      checkRole([ERole.umanuAdmin]),
+      this.controller.updateOrganization
+    );
+  }
 }
--- src/routes/tenant.route.ts
+++ src/routes/tenant.route.ts
@@ -1,23 +1,48 @@
-import { Router } from 'express';
 import { TenantController } from '@/controllers/tenant.controller';
 import { checkAuth } from '@/middleware/auth.middleware';
 import { checkRole } from '@/middleware/roleChecker.middleware';
+import { checkEntityAccess } from '@/middleware/updateChecker.midleware';
 import { ERole } from '@/types/roles';
+import { Router } from 'express';
 
 export class TenantRoute {
-    public path = '/tenants';
-    public router = Router();
-    private controller: TenantController;
+  public path = "/tenants";
+  public router = Router();
+  private controller: TenantController;
 
-    constructor() {
-        this.controller = new TenantController();
-        this.init();
-    }
+  constructor() {
+    this.controller = new TenantController();
+    this.init();
+  }
 
-    private init() {
-        this.router.post('/', checkAuth, checkRole([ERole.umanuAdmin, ERole.organizationAdmin]), this.controller.createTenantEntry);
-        this.router.get('/:id', checkAuth, this.controller.getTenantById);
-        this.router.get('/', checkAuth, this.controller.getAllTenants);
-        this.router.put('/:id', checkAuth, this.controller.updateTenant);
-    }
+  private init() {
+    this.router.post(
+      "/",
+      checkAuth,
+      checkRole([
+        ERole.umanuAdmin,
+        ERole.organizationAdmin,
+        ERole.buildingAdmin
+      ]),
+      this.controller.createTenantEntry
+    );
+    this.router.get(
+      "/:id",
+      checkAuth,
+      checkEntityAccess("tenant"),
+      this.controller.getTenantById
+    );
+    this.router.get("/", checkAuth, this.controller.getAllTenants);
+    this.router.put(
+      "/:id",
+      checkAuth,
+      checkRole([
+        ERole.umanuAdmin,
+        ERole.organizationAdmin,
+        ERole.buildingAdmin
+      ]),
+      checkEntityAccess('tenant'),
+      this.controller.updateTenant
+    );
+  }
 }
